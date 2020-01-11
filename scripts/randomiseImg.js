2e319eb7f953e4b87cbacbb76ae6942cb1c960f8
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2020-01-11 19:39:07 -0300

    working the imgHead randomization

--- scripts/randomiseImg.js
+++ scripts/randomiseImg.js
@@ -1,4 +1,4 @@
-function randImgHeader (number) {
+function randImgHeader(number) {
     let randNum = Math.floor(Math.random() * number) + 1;
     console.log(randNum);
     document.getElementById("imgHead").src = "../outros/images/headers/head" + randNum + ".png";
