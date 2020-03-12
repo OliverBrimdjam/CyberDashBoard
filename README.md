fb426dd55505a4da11a3cdd907b45f97b43c84d4
Oliver Brimdjam <oliverbrimdjam@hotmail.com>
2020-03-12 10:08:37 -0300

    preparing React version

--- README.md
+++
@@ -1,2 +0,0 @@
-# CyberDashBoard
-iniciativa de criar uma ferramenta computacional que automatize muitas funções de uma partida de CyberPunk 2020
--- scripts/randomiseImg.js
+++
@@ -1,6 +0,0 @@
-function randImgHeader(number) {
-    let randNum = Math.floor(Math.random() * number) + 1;
-    console.log(randNum);
-    document.getElementById("imgHead").src = "../outros/images/headers/head" + randNum + ".png";
-}
-
