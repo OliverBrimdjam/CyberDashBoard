2f6f47733f219639e8c81f533c30cafc20752983
Igor Britto <igor.galvao@gmail.com>
2020-04-30 22:13:21 -0300

    Abstract DamageRow

--- src/health/HealthPatch.js
+++ src/health/HealthPatch.js
@@ -26,90 +26,194 @@ var setRlegDmg;
 var lLegDmg;
 var setLlegDmg;
 
+function DamageRow({ labelName, value, setter, outraCoisa }) {
+  return (
+    <div>
+      <label>{labelName}</label>
+      <label>{value}</label>
+      <button
+        onClick={() => {
+          setter(value + 1);
+          outraCoisa();
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          setter(value - 1);
+          outraCoisa();
+        }}
+      >
+        -
+      </button>
+    </div>
+  );
+}
 
+export function GeneralHealth(props) {
+  generalHealth = props.lifeInfo;
 
+  //situation states
+  stunStat = props.stunStat;
+  setStun = props.setStun;
+  woundLife = props.woundLife;
+  setWound = props.setWound;
 
-export function GeneralHealth(props){
-    generalHealth = props.lifeInfo;
-    
-    //situation states
-    stunStat = props.stunStat;
-    setStun = props.setStun;
-    woundLife = props.woundLife;
-    setWound = props.setWound;
+  //damage states
+  headDmg = props.headDmg;
+  setHeadDmg = props.setHeadDmg;
+  torsoDmg = props.torsoDmg;
+  setTorsoDmg = props.setTorsoDmg;
+  rArmDmg = props.rArmDmg;
+  setRarmDmg = props.setRarmDmg;
+  lArmDmg = props.lArmDmg;
+  setLarmDmg = props.setLarmDmg;
+  rLegDmg = props.rLegDmg;
+  setRlegDmg = props.setRlegDmg;
+  lLegDmg = props.lLegDmg;
+  setLlegDmg = props.setLlegDmg;
 
-    //damage states
-    headDmg = props.headDmg;
-    setHeadDmg = props.setHeadDmg;
-    torsoDmg = props.torsoDmg;
-    setTorsoDmg = props.setTorsoDmg;
-    rArmDmg = props.rArmDmg;
-    setRarmDmg = props.setRarmDmg;
-    lArmDmg = props.lArmDmg;
-    setLarmDmg = props.setLarmDmg;
-    rLegDmg = props.rLegDmg;
-    setRlegDmg = props.setRlegDmg;
-    lLegDmg = props.lLegDmg;
-    setLlegDmg = props.setLlegDmg;
+  const [totalDmgS, setTotalDmg] = React.useState(0);
 
-    const [totalDmgS, setTotalDmg] = React.useState(0);
-
-    function healthChangeEvento(){
-        
-        console.log("direitosIguais" );
-         totalDmg = headDmg + torsoDmg + rArmDmg + lArmDmg + rLegDmg + lLegDmg;
-         setTotalDmg(totalDmg); 
-         console.log("vai vai carai!");
-    
-    }
-    return (
-        <div id="healthContainer" onChange={healthChangeEvent}>
-            <div>
-                <label>Wound Status</label>
-                <label>{woundLife}</label>
-                <button onClick={() => setWound("varvarvar")}>change status</button>
-            </div>
-            <div>
-                <label>Total Dmg</label>
-                <label>{totalDmgS}</label>
-            </div>
-            <div>
-                <label>Head</label>
-                <label >{headDmg}</label>
-                <button onClick={() => {setHeadDmg(headDmg + 1); healthChangeEvento()}} onChange={healthChangeEvento} >+</button>
-                <button onClick={() => {setHeadDmg(headDmg - 1); healthChangeEvento()}}>-</button>
-            </div>
-            <div>
-                <label>Torso</label>
-                <label>{torsoDmg}</label>
-                <button onClick={() => {setTorsoDmg(torsoDmg +1); healthChangeEvento()}}>+</button>
-                <button onClick={() => {setTorsoDmg(torsoDmg -1); healthChangeEvento()}}>-</button>
-            </div>
-            <div>
-                <label>R.Arm</label>
-                <label>{rArmDmg}</label>
-                <button onClick={() => {setRarmDmg(rArmDmg +1); healthChangeEvento()}}>+</button>
-                <button onClick={() => {setRarmDmg(rArmDmg -1); healthChangeEvento()}}>-</button>
-            </div>
-            <div>
-                <label>L.Arm</label>
-                <label>{lArmDmg}</label>
-                <button onClick={() => {setLarmDmg(lArmDmg +1); healthChangeEvento()}}>+</button>
-                <button onClick={() => {setLarmDmg(lArmDmg -1); healthChangeEvento()}}>-</button>
-            </div>
-            <div>
-                <label>R.Leg</label>
-                <label>{rLegDmg}</label>
-                <button onClick={() => {setRlegDmg(rLegDmg +1); healthChangeEvento()}}>+</button>
-                <button onClick={() => {setRlegDmg(rLegDmg -1); healthChangeEvento()}}>-</button>
-            </div>
-            <div>
-                <label>L.Leg</label>
-                <label>{lLegDmg}</label>
-                <button onClick={() => {setLlegDmg(lLegDmg +1); healthChangeEvento()}}>+</button>
-                <button onClick={() => {setLlegDmg(lLegDmg -1); healthChangeEvento()}}>-</button>
-            </div>
-        </div>
-    );
-} 
-
+  function healthChangeEvento() {
+    console.log("direitosIguais");
+    totalDmg = headDmg + torsoDmg + rArmDmg + lArmDmg + rLegDmg + lLegDmg;
+    setTotalDmg(totalDmg);
+    console.log("vai vai carai!");
+  }
+  return (
+    <div id="healthContainer" onChange={healthChangeEvent}>
+      <div>
+        <label>Wound Status</label>
+        <label>{woundLife}</label>
+        <button onClick={() => setWound("varvarvar")}>change status</button>
+      </div>
+      <div>
+        <label>Total Dmg</label>
+        <label>{totalDmgS}</label>
+      </div>
+      <div>
+        <label>Head</label>
+        <label>{headDmg}</label>
+        <button
+          onClick={() => {
+            setHeadDmg(headDmg + 1);
+            healthChangeEvento();
+          }}
+          onChange={healthChangeEvento}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            setHeadDmg(headDmg - 1);
+            healthChangeEvento();
+          }}
+        >
+          -
+        </button>
+      </div>
+      <div>
+        <label>Torso</label>
+        <label>{torsoDmg}</label>
+        <button
+          onClick={() => {
+            setTorsoDmg(torsoDmg + 1);
+            healthChangeEvento();
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            setTorsoDmg(torsoDmg - 1);
+            healthChangeEvento();
+          }}
+        >
+          -
+        </button>
+      </div>
+      <div>
+        <label>R.Arm</label>
+        <label>{rArmDmg}</label>
+        <button
+          onClick={() => {
+            setRarmDmg(rArmDmg + 1);
+            healthChangeEvento();
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            setRarmDmg(rArmDmg - 1);
+            healthChangeEvento();
+          }}
+        >
+          -
+        </button>
+      </div>
+      <div>
+        <label>L.Arm</label>
+        <label>{lArmDmg}</label>
+        <button
+          onClick={() => {
+            setLarmDmg(lArmDmg + 1);
+            healthChangeEvento();
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            setLarmDmg(lArmDmg - 1);
+            healthChangeEvento();
+          }}
+        >
+          -
+        </button>
+      </div>
+      <div>
+        <label>R.Leg</label>
+        <label>{rLegDmg}</label>
+        <button
+          onClick={() => {
+            setRlegDmg(rLegDmg + 1);
+            healthChangeEvento();
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            setRlegDmg(rLegDmg - 1);
+            healthChangeEvento();
+          }}
+        >
+          -
+        </button>
+      </div>
+      <div>
+        <label>L.Leg</label>
+        <label>{lLegDmg}</label>
+        <button
+          onClick={() => {
+            setLlegDmg(lLegDmg + 1);
+            healthChangeEvento();
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            setLlegDmg(lLegDmg - 1);
+            healthChangeEvento();
+          }}
+        >
+          -
+        </button>
+      </div>
+    </div>
+  );
+}
