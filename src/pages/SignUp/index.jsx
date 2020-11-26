8be82d7d7adaccaa44489178430d8b9365031576
OliverBrimdjam <oliverbrimdjam@hotmail.com>
2020-11-26 17:04:42 -0300

    signup page input style ready

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
 import './styles.css';
 
+import mainLogo from '../../assets/logo/cyberLogo_1.png';
+// import moldura from '../../assets/inputs/input_2.png';
+
 const SignUp = () => {
-  return <h1>olha</h1>;
+  return (
+    <div className="main">
+      <section className="main__content">
+        <img src={mainLogo} alt="logo cyber dashboard" className="logo" />
+        <form action="" className="formBlock">
+          <div className="inputContainer">
+            <input
+              type="text"
+              className="inputContainer__input"
+              placeholder="Nome"
+            />
+          </div>
+          <div className="inputContainer">
+            <input
+              type="email"
+              className="inputContainer__input"
+              placeholder="E-mail"
+            />
+          </div>
+          <div className="inputContainer">
+            <input
+              type="password"
+              className="inputContainer__input"
+              placeholder="Senha"
+            />
+          </div>
+          <div className="inputContainer">
+            <input
+              type="password"
+              className="inputContainer__input"
+              placeholder="Confirmar Senha"
+            />
+          </div>
+          <button type="submit" className="confirm">
+            Cadastrar
+          </button>
+        </form>
+      </section>
+      <section className="wallpaper" />
+    </div>
+  );
 };
 
 export default SignUp;
